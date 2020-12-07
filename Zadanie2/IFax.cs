eb6d18e2838157e813d10642b6dc997cf772fc6e
miceyjuice <ganjafz@gmail.com>
2020-12-07 21:52:58 +0100

    Task two almost done

---
+++ Zadanie2/IFax.cs
@@ -0,0 +1,15 @@
+﻿using Zadanie1;
+
+namespace Zadanie2
+{
+    public interface IFax : IDevice
+    {
+            /// <summary>
+            /// Dokument jest wysyłany, jeśli urządzenie włączone. W przeciwnym przypadku nic się nie wykonuje
+            /// </summary>
+            /// <param name="document">obiekt typu IDocument, różny od `null`</param>
+            /// <param name="faxNumber">obiekt typu String, różny od `null`</param>
+            
+        void Send(IDocument document, string faxNumber);
+    }
+}
--- Zadanie2/MultifunctionalDevice.cs
+++ Zadanie2/MultifunctionalDevice.cs
@@ -1,36 +1,70 @@
-﻿using System;
-using Zadanie1;
+﻿using Zadanie1;
+using System;
 
-namespace Zadanie1
+namespace Zadanie2
 {
-    public class Copier : BaseDevice, IScanner, IPrinter
+    public class MultifunctionalDevice : BaseDevice, IPrinter, IScanner, IFax
     {
         private int _printCounter = 0;
         private int _scanCounter = 0;
+        private int _sendCounter = 0;
         private int _counter = 0;
+        private string _faxNumber;
 
         public int PrintCounter
         {
             get => _printCounter;
-            private set => _printCounter = value;
+            set => _printCounter = value;
         }
 
         public int ScanCounter
         {
             get => _scanCounter;
-            private set => _scanCounter = value;
+            set => _scanCounter = value;
         }
 
-        public new int Counter
+        public int SendCounter
+        {
+            get => _sendCounter;
+            set => _sendCounter = value;
+        }
+
+        public int Counter
         {
             get => _counter;
             private set => _counter = value;
+        }
+
+        public string FaxNumber
+        {
+            get => _faxNumber;
+            set => _faxNumber = value;
+        }
+
+        public MultifunctionalDevice(string faxNumber)
+        {
+            FaxNumber = faxNumber;
         }
 
         public void PowerOn()
         {
             _counter++;
             base.PowerOn();
+        }
+
+        public void Print(in IDocument document)
+        {
+            switch (state)
+            {
+                case IDevice.State.@on:
+                    _printCounter++;
+                    Console.WriteLine($"{DateTime.Now} Print: {document.GetFileName()}");
+                    break;
+                case IDevice.State.off:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
 
         public void Scan(out IDocument document, IDocument.FormatType formatType = IDocument.FormatType.JPG)
@@ -42,7 +76,7 @@ namespace Zadanie1
                 _ => "Image"
             };
 
-            string name = $"{scanDocumentType}Scan{ScanCounter + 1}.{formatType.ToString().ToLower()}";
+            var name = $"{scanDocumentType}Scan{ScanCounter + 1}.{formatType.ToString().ToLower()}";
 
             if (formatType == IDocument.FormatType.PDF)
                 document = new PDFDocument(name);
@@ -54,23 +88,8 @@ namespace Zadanie1
             switch (state)
             {
                 case IDevice.State.@on:
-                    ScanCounter++;
+                    _scanCounter++;
                     Console.WriteLine($"{DateTime.Now} Scan: {document.GetFileName()}");
-                    break;
-                case IDevice.State.off:
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
-
-        public void Print(in IDocument document)
-        {
-            switch (state)
-            {
-                case IDevice.State.@on:
-                    PrintCounter++;
-                    Console.WriteLine($"{DateTime.Now} Print: {document.GetFileName()}");
                     break;
                 case IDevice.State.off:
                     break;
@@ -81,10 +100,25 @@ namespace Zadanie1
 
         public void ScanAndPrint()
         {
-            Scan(out IDocument doc);
-            Print(doc);
+            Scan(out IDocument document);
+            Print(document);
             ScanCounter++;
             PrintCounter++;
+        }
+
+        public void Send(IDocument doc, string faxNumber)
+        {
+            switch (state)
+            {
+                case IDevice.State.@on:
+                    _sendCounter++;
+                    Console.WriteLine($"{DateTime.Now} Sent: {doc.GetFileName()} from: {this._faxNumber} to: {faxNumber}");
+                    break;
+                case IDevice.State.off:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
     }
 }
